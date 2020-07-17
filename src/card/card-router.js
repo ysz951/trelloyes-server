424d6b1be7ba7f70c054bce129906127b84a94dd
ysz951 <shengyang_zhou@baylor.edu>
2020-07-17 00:43:42 -0500

    add list-router

--- src/card/card-router.js
+++ src/card/card-router.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const { v4: uuid } = require('uuid')
+const logger = require('../logger')
+const { cards, lists } = require('../store')
+
 const cardRouter = express.Router()
 const bodyParser = express.json()
-const cards = [{
-    id: 1,
-    title: 'Task One',
-    content: 'This is card one'
-}]
 
 cardRouter
   .route('/card')
@@ -39,7 +37,6 @@ cardRouter
 
     cards.push(card);
     logger.info(`Card with id ${id} created`);
-
     res
         .status(201)
         .location(`http://localhost:8000/card/${id}`)
@@ -49,10 +46,45 @@ cardRouter
 cardRouter
   .route('/card/:id')
   .get((req, res) => {
-    // move implementation logic into here
+    const { id } = req.params;
+    const card = cards.find(c => c.id == id);
+  
+    // make sure we found a card
+    if (!card) {
+      logger.error(`Card with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Card Not Found');
+    }
+  
+    res.json(card);
   })
   .delete((req, res) => {
-    // move implementation logic into here
+    const { id } = req.params;
+
+    const cardIndex = cards.findIndex(c => c.id == id);
+
+    if (cardIndex === -1) {
+        logger.error(`Card with id ${id} not found.`);
+        return res
+        .status(404)
+        .send('Not found');
+    }
+
+    //remove card from lists
+    //assume cardIds are not duplicated in the cardIds array
+    lists.forEach(list => {
+        const cardIds = list.cardIds.filter(cid => cid !== id);
+        list.cardIds = cardIds;
+    });
+
+    cards.splice(cardIndex, 1);
+
+    logger.info(`Card with id ${id} deleted.`);
+
+    res
+        .status(204)
+        .end();
   })
 
 module.exports = cardRouter
